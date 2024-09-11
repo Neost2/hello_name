5e4c01be00689178e392dbe83c056aa6a9fcd11c
Neost2 <Neost2@hotmail.com>
2024-09-10 22:42:44 -0500

    Simple input output code seeing if I have right for it or not. getting c and C++ mixed up.

---
+++ hello_name.c
@@ -0,0 +1,18 @@
+/* hello_name */
+
+#include <stdio.h>
+
+
+
+
+longint main(void)
+{
+	//get name
+	    char name[20];
+		printf ("Please enter in your name then [return]: ");
+		scanf(% s, name);
+		printf ("Hello, %s!\n, name ");
+
+		return 0;
+
+}
